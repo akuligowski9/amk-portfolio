b31fc84419959c47ad6ae096ff3a0390f15bee9f
Alex Kuligowski <alekuligowski@gmail.com>
2026-01-01 12:45:24 -0500

    add analytics from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NavBar } from "@/components/site/navbar";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -20,6 +21,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       >
         <NavBar />
         {children}
+
+        <Analytics />
       </body>
     </html>
   );
