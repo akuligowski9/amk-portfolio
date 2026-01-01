835aa2e81e4982cac0e68b4fa70c6d3e3f8cc4d8
Alex Kuligowski <alekuligowski@gmail.com>
2026-01-01 12:50:53 -0500

    add speed-insights from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NavBar } from "@/components/site/navbar";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -23,6 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {children}
 
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
